8031e6159cfe6549fff4ff4825dd2ade6738510f
xatanical <safestock50@gmail.com>
2025-12-02 12:49:47 +0700

    Update clover.js

--- clover.js
+++ clover.js
@@ -1548,7 +1548,7 @@ if (!groupOnly(ctx)) return;
 
     const menuCaption = `<blockquote>  
  T R E D I C T I N V I C T U S
-       V25 • GEN VI
+       V399 • GEN VI
 
 👑 Owner : @Xatanicvxii  
 💮 Partner : Bella
